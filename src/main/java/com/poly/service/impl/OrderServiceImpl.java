9ab48b13510d650ef7ad5e67472a50cbe99d729e
GiaKhanh17 <117286737+GiaKhanh17@users.noreply.github.com>
2025-02-25 16:35:07 +0700

    first commit

--- src/main/java/com/poly/service/impl/OrderServiceImpl.java
+++ src/main/java/com/poly/service/impl/OrderServiceImpl.java
@@ -25,15 +25,18 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Orders create(JsonNode orderData) {
-        // Tạo một đối tượng ObjectMapper để chuyển đổi dữ liệu từ JsonNode sang đối tượng Orders
+        // Tạo một đối tượng ObjectMapper để chuyển đổi dữ liệu từ JsonNode sang đối
+        // tượng Orders
         ObjectMapper mapper = new ObjectMapper();
 
         // Chuyển đổi dữ liệu từ JsonNode sang đối tượng Orders và lưu vào cơ sở dữ liệu
         Orders order = mapper.convertValue(orderData, Orders.class);
         dao.save(order);
 
-        // Chuyển đổi dữ liệu từ JsonNode sang danh sách OrdersDetail và lưu vào cơ sở dữ liệu
-        TypeReference<List<OrdersDetail>> type = new TypeReference<List<OrdersDetail>>() {};
+        // Chuyển đổi dữ liệu từ JsonNode sang danh sách OrdersDetail và lưu vào cơ sở
+        // dữ liệu
+        TypeReference<List<OrdersDetail>> type = new TypeReference<List<OrdersDetail>>() {
+        };
         List<OrdersDetail> details = mapper.convertValue(orderData.get("ordersDetail"), type)
                 .stream().peek(d -> d.setOrderId(order)).collect(Collectors.toList());
         dtDao.saveAll(details);
@@ -47,16 +50,15 @@ public class OrderServiceImpl implements OrderService {
         return dao.findById(id).orElse(null);
     }
 
+    @Override
+    public List<Orders> findByUsername(String username) {
+        // Tìm kiếm danh sách đơn hàng dựa trên tên đăng nhập và trả về kết quả
+        return dao.finByUsername(username);
+    }
 
-	  @Override
-	    public List<Orders> findByUsername(String username) {
-		// Tìm kiếm danh sách đơn hàng dựa trên tên đăng nhập và trả về kết quả 
-	        return dao.finByUsername(username);
-	    } 
-
-	/*
-	 * @Override public List<Orders> findByUsername(String username) { // Tìm kiếm
-	 * danh sách đơn hàng dựa trên tên đăng nhập và trả về kết quả return
-	 * dao.findByUsername(username); }
-	 */
+    /*
+     * @Override public List<Orders> findByUsername(String username) { // Tìm kiếm
+     * danh sách đơn hàng dựa trên tên đăng nhập và trả về kết quả return
+     * dao.findByUsername(username); }
+     */
 }
